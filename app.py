a20d25a56c475b0505f7ce5041608bf12c5bad32
Harsh Malik <116074963+harshmalik24@users.noreply.github.com>
2023-02-27 00:26:33 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 import streamlit as st
-
+import os
+from matplotlib import image
 # Set page title and icon
 st.set_page_config(page_title="My Streamlit App", page_icon=":wave:")
-
-# Define the greeting message
-message = "Hello everyone, welcome to my Streamlit app! You can find me on LinkedIn by clicking the link below:"
+message = "Hello everyone :wave:"
+st.title(message)
+st.header("Welcome to my Streamlit app! You can find me on :blue[LinkedIn] by clicking the link below:")
 
 # Define the LinkedIn URL
 linkedin_url = "www.linkedin.com/in/harshmalik424"
-
-# Add a header with the greeting message
-st.header(message)
-
 # Add a link to your LinkedIn profile
-st.markdown(f'<a href="{linkedin_url}" style="font-size: 24px;">LinkedIn</a>', unsafe_allow_html=True)
+st.markdown(f'<a href="{linkedin_url}" style="font-size: 30px;">LinkedIn</a>', unsafe_allow_html=True)
 
 # Apply some styling to the link and background color
 st.markdown(
@@ -33,3 +30,19 @@ st.markdown(
     """,
     unsafe_allow_html=True,
 )
+st.subheader("Lets connect and grow together! :sunglasses:")
+
+btn_click = st.button("Click Me!")
+
+
+
+image_dir = 'C:/Users/harsh/Desktop/Intern/project2/resources/images/f_img.jpg'
+image_name = 'f_img.jpsg'
+image_path = os.path.join(image_dir, image_name)
+
+
+
+
+if btn_click == True:
+    # Display the image using Streamlit
+    st.image(image_path, use_column_width=True)
